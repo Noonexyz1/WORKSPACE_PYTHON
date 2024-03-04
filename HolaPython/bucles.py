bb004d372c433949db6c1e2bb084fb50ffee0eef
Noonexyz1 <aries-d2github@hotmail.com>
2024-03-04 14:50:48 -0400

    Fors en python aprendido

---
+++ HolaPython/bucles.py
@@ -0,0 +1,31 @@
+#while
+# Create the variable for user input
+user_input = ''
+# Create the list to store the values
+inputs = []
+
+# The while loop
+while user_input.lower() != 'done':
+    # Check if there's a value in user_input
+    if user_input:
+        # Store the value in the list
+        inputs.append(user_input)
+    # Prompt for a new value
+    user_input = input('Enter a new value, or done when done')
+
+
+
+new_planet = ''
+planets = []
+while new_planet.lower() != 'done':
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input('Enter a new planet, or done if done')
+
+print(planets)
+
+#for
+countdown = [4, 3, 2, 1, 0]
+for number in countdown:
+    print(number)
+print("Blast off!! 🚀")
